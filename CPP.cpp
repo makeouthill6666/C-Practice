ca82453529c33318caf2ba1cbd805551955b84ea
makeouthill6666 <superduperrookie@gmail.com>
2024-01-26 10:01:39 +0900

    v0.4

--- CPP.cpp
+++ CPP.cpp
@@ -321,25 +321,49 @@
 //     cout << "전체 급여 : " << setprecision(2) << totalpay << endl;
 //     return 0;
 // }
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int day;
+
+//     cout << "0~6 범위의 정수";
+//     cin >> day;
+
+//     switch(day)
+//     {
+//         case 0 : cout << "일요일" << endl;
+//         case 1 : cout << "월요일" << endl;
+//         case 2 : cout << "화요일" << endl;
+//         case 3 : cout << "수요일" << endl;
+//         case 4 : cout << "목요일" << endl;
+//         case 5 : cout << "금요일" << endl;
+//         case 6 : cout << "토요일" << endl;
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int day;
+    char grade;
 
-    cout << "0~6 범위의 정수";
-    cin >> day;
+    cout << "학점 A~F : ";
+    cin >> grade;
 
-    switch(day)
+    switch(grade)
     {
-        case 0 : cout << "일요일" << endl;
-        case 1 : cout << "월요일" << endl;
-        case 2 : cout << "화요일" << endl;
-        case 3 : cout << "수요일" << endl;
-        case 4 : cout << "목요일" << endl;
-        case 5 : cout << "금요일" << endl;
-        case 6 : cout << "토요일" << endl;
+        case 'A' :
+        case 'B' :
+        case 'C' : cout << "pass";
+                break;
+        case 'D' :
+        case 'F' : cout << "fail";
+                   break;
+        default : cout << "invalid";
     }
     return 0;
 }
