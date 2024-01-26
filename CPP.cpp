eec153e2fc57c80b63468693306338514f4a4b01
makeouthill6666 <superduperrookie@gmail.com>
2024-01-26 12:52:27 +0900

    v0.6 인강 공부

--- CPP.cpp
+++ CPP.cpp
@@ -385,30 +385,20 @@
 //     return 0;
 // }
 
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
-    int lower = 1;
-    int higher = 300;
-    int divisor = 7;
+    int startday;
+    int daysinmonth;
     int col = 1;
 
-    for(int i = lower; i < higher ; i++)
+    do
     {
-        if(i % divisor == 0)
-        {
-            cout << setw(4) << i;
-            col++;
-            if(col>10)
-            {
-                cout << endl;
-                col = 1;
-            }
-        }
-
-    }
-    return 0;
+        /* code */
+    } while (/* condition */);
+    
 }
---
+++ 따배씨.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+    int x = 2;
+    x = 5;
+    int y = x + 3;
+
+    std::cout << 1 + 2 << std::endl;
+
+    return 0;
+}
