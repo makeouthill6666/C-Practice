8d1da8c710cddc7588e3001337d33749bd8bcd53
makeouthill6666 <superduperrookie@gmail.com>
2024-01-26 18:05:40 +0900

    v1.3 지역 범위!!

--- 따배씨.cpp
+++ 따배씨.cpp
@@ -142,26 +142,48 @@
 // }
 
 
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int x = 0; // int x(0);
+
+//     cout << x << " " << &x << endl;
+
+//     // int x = 1;
+
+//     {
+//         // int x = 1;
+//         x  = 1;
+
+//         cout << x << " " << &x << endl;
+//     }
+//     cout << x << " " << &x << endl;
+
+ 
+//     return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
 
+void doSomething(int x)
+{
+    x = 123;
+    cout << x << endl; //123 
+}
+
 int main()
 {
-    int x = 0; // int x(0);
+    int x = 0;
+    cout << x << endl; //0
+    doSomething(x);
+    cout << x << endl; //123
 
-    cout << x << " " << &x << endl;
-
-    // int x = 1;
-
-    {
-        // int x = 1;
-        x  = 1;
-
-        cout << x << " " << &x << endl;
-    }
-    cout << x << " " << &x << endl;
-
- 
     return 0;
 }
+
+//다 틀림
