4861f679966f427de529f0c77dfd8597cab6360f
makeouthill6666 <superduperrookie@gmail.com>
2024-02-01 17:57:21 +0900

    v2.4 odd even

--- 따배씨.cpp
+++ 따배씨.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <iomanip>
-#include <limits>
-#include <cmath>
 
 int main()
 {
     using namespace std;
 
-    double zero = 0.0;
-    double posinf = 5.0 / zero;
-    double neginf = -5.0 / zero;
-    double nan = zero / zero;
-
-    cout << posinf << " " << std::isinf(posinf) << endl; //inf   
-    cout << neginf << " " << std::isinf(neginf) << endl; //-inf
-    cout << nan << " " << std::isnan(nan) << endl; //-nan(indeterminate)
-    cout << 1.0 << " " << std::isnan(1.0) << endl; //-nan(indeterminate)
+    int number;
     
+    cout << "numbers";
+    cin >> number;
 
 
+     if(number %2 == 1)
+    {
+        cout << "홀수" << endl;
+    }
+
+    else
+        cout << "짝수" << endl;
 
     return 0;
 }
+
