e5ee5736ae92f047fbfd3c7158db6e7f1c534bd4
makeouthill6666 <superduperrookie@gmail.com>
2024-02-01 16:17:25 +0900

    v2.3

--- 따배씨.cpp
+++ 따배씨.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <iomanip>
 #include <limits>
+#include <cmath>
 
 int main()
 {
     using namespace std;
 
-    double d1(1.0);
-    double d2(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
-    
-    cout << setprecision(17);
+    double zero = 0.0;
+    double posinf = 5.0 / zero;
+    double neginf = -5.0 / zero;
+    double nan = zero / zero;
 
-    cout << d1 << endl;
-    cout << d2 << endl;
+    cout << posinf << " " << std::isinf(posinf) << endl; //inf   
+    cout << neginf << " " << std::isinf(neginf) << endl; //-inf
+    cout << nan << " " << std::isnan(nan) << endl; //-nan(indeterminate)
+    cout << 1.0 << " " << std::isnan(1.0) << endl; //-nan(indeterminate)
+    
+
+
 
     return 0;
 }
