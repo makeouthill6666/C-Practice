0163bf7860ffb37665918c75ac1605107a3bdf60
makeouthill6666 <superduperrookie@gmail.com>
2024-02-02 11:09:58 +0900

    v2.5 casting

--- 따배씨.cpp
+++ 따배씨.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
     using namespace std;
 
-    int number;
+    // char c1(890);
+    // char c2('A');
     
-    cout << "numbers";
-    cin >> number;
+    // cout << c1 << " " << c2 << " " << int(c1) << " " << int(c2) << endl;
+    // //c style
+    // cout << (char)65 << endl;
+    // cout << (int)'A' << endl;
+    // //c++ style
+    // cout << char(65) << endl;
+    // cout << int('A') << endl;
+
+//    cin >> c1;
+//    cout << c1 << " " << int(c1) << endl;
+
+//    cin >> c1;
+//    cout << c1 << " " << int(c1) << endl;
+
+//    cin >> c1;
+//    cout << c1 << " " << int(c1) << endl;
+
+//    cin >> c1;
+//    cout << c1 << " " << int(c1) << endl;
 
 
-     if(number %2 == 1)
-    {
-        cout << "홀수" << endl;
-    }
+char c1(65);
 
-    else
-        cout << "짝수" << endl;
+cout << sizeof(unsigned char) << endl;
+cout << (int)std::numeric_limits<unsigned char>::max() << endl;
+cout << (int)std::numeric_limits<unsigned char>::lowest() <<endl;
 
-    return 0;
+
+return 0;
 }
-
