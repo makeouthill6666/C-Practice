be8e4d8291ba3db320ce5965eb7badfca0afb57a
makeouthill6666 <superduperrookie@gmail.com>
2024-02-05 10:41:27 +0900

    os v0.2 execl

--- 따배씨.cpp
+++ 따배씨.cpp
@@ -1,41 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
 
 int main()
 {
-   int pid;
-   int counter = 1;
-   pid = fork();
-   if (pid<0)
-   {
-      fprintf(stderr,"Fork Failed");
-      exit(-1);
-   }
-   else if (pid==0)
-   {
-      printf("Child here!\n");
-      while (counter <6)
-      {
-         printf("Child num :%d\n", counter);
-
-         counter++;
-         sleep(1);
-      }
-   }
-   else
-   {
-      printf("i'm parent\n");
-      while(counter <6)
-      {
-         printf("parent num: %d\n", counter);
-         counter ++;
-         sleep(1);
-      }
-      wait(NULL);
-      printf("process Complete\n");
-   }
-
+   printf("execl start/n");
+   execl("/bin/ls", "ls", "-l", NULL);
+   printf("if you read this message, fail creating execl func../n");
    return 0;
 }
